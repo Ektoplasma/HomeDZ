65dcf45aa989145b51ce19e539495e5c3d492513
SlyhLeZwify <rudy.canesse@insa-cvl.fr>
2017-02-06 16:34:24 +0100

    Ajout onglet Meteo en dur

---
+++ app/src/main/java/insacvl/sti/ssu/homedz/ItemListBaseAdapterMeteo.java
@@ -0,0 +1,64 @@
+package insacvl.sti.ssu.homedz;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ItemListBaseAdapterMeteo extends BaseAdapter {
+
+    private Context context = null;
+    private static ArrayList<ItemDetails> itemDetailsrrayList;
+    private LayoutInflater l_Inflater;
+
+    public ItemListBaseAdapterMeteo(Context context, ArrayList<ItemDetails> results) {
+        this.context = context;
+        itemDetailsrrayList = results;
+        l_Inflater = LayoutInflater.from(context);
+    }
+
+    public int getCount() {
+        return itemDetailsrrayList.size();
+    }
+
+    public Object getItem(int position) {
+        return itemDetailsrrayList.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if (convertView == null) {
+            convertView = l_Inflater.inflate(R.layout.item_details_view_meteo, null);
+            holder = new ViewHolder();
+            holder.img_item = (ImageView) convertView.findViewById(R.id.photo);
+            holder.txt_desc = (TextView) convertView.findViewById(R.id.desc);
+            holder.txt_val = (TextView) convertView.findViewById(R.id.val);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.img_item.setImageResource(itemDetailsrrayList.get(position).getImageNumber());
+        holder.txt_desc.setText(itemDetailsrrayList.get(position).getDesc());
+        holder.txt_val.setText(Integer.toString(itemDetailsrrayList.get(position).getVal()));
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView img_item;
+        TextView txt_desc;
+        TextView txt_val;
+    }
+}
--- app/src/main/java/insacvl/sti/ssu/homedz/MeteoFragment.java
+++ app/src/main/java/insacvl/sti/ssu/homedz/MeteoFragment.java
@@ -5,18 +5,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MeteoFragment extends Fragment {
+
+    private ListView lv1 = null;
+    private ArrayList<ItemDetails> tableau;
 
     public MeteoFragment(){
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.z_fragment_meteo_layout, container, false);
-        TextView txt = (TextView) rootView.findViewById(R.id.tvMeteo);
+        View rootView = inflater.inflate(R.layout.activity_list_obat, container, false);
+
+        tableau = new ArrayList<ItemDetails>();
+
+        lv1 = (ListView) rootView.findViewById(R.id.listView);
+        lv1.setAdapter(new ItemListBaseAdapterMeteo(getContext(), GetSearchResults()));
+
         return rootView;
+    }
+
+    private ArrayList<ItemDetails> GetSearchResults() {
+        ArrayList<ItemDetails> results = new ArrayList<ItemDetails>();
+
+        ItemDetails item_details = new ItemDetails();
+        item_details.setDesc("Bourges");
+        item_details.setVal(8);
+        item_details.setImageNumber(R.drawable.rain);
+        results.add(item_details);
+
+        return results;
     }
 }
